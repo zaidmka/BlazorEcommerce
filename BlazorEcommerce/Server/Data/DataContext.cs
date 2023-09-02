052a4809608e60e3c781c80ed1eb099f3640005e
zaidmka <zaid.mka.90@gmail.com>
2023-09-02 18:07:38 +0300

    error ef

---
+++ BlazorEcommerce/Server/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorEcommerce.Server.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- BlazorEcommerce/Shared/Product.cs
+++ BlazorEcommerce/Shared/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ namespace BlazorEcommerce.Shared
 
         public string Description { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
