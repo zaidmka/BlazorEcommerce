1319834f3e40a981303d1d0d31eeadc38f7219e9
zaidmka <zaid.mka.90@gmail.com>
2023-09-02 20:59:19 +0300

    finish the interface!

--- BlazorEcommerce/Server/Program.cs
+++ BlazorEcommerce/Server/Program.cs
@@ -1,5 +1,6 @@
 global using BlazorEcommerce.Shared;
 global using Microsoft.EntityFrameworkCore;
+global using BlazorEcommerce.Server.Services.ProductService;
 using BlazorEcommerce.Server.Data;
 using Microsoft.AspNetCore.ResponseCompression;
 
@@ -13,6 +14,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IProductServicecs, ProductService>();
 var app = builder.Build();
 app.UseSwaggerUI();
 
---
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -0,0 +1,22 @@
+﻿using BlazorEcommerce.Server.Data;
+
+namespace BlazorEcommerce.Server.Services.ProductService
+{
+    public class ProductService : IProductServicecs
+    {
+        private readonly DataContext _context;
+
+        public ProductService(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<ServiceResponse<List<Product>>> GetProductsAsync()
+        {
+            var response = new ServiceResponse<List<Product>>()
+            {
+                Data = await _context.Products.ToListAsync()
+            };
+            return response;
+        }
+    }
+}
