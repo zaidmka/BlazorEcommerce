0eddeee574803c589c2358abd88780ddc9e1b181
zaidmka <zaid.mka.90@gmail.com>
2023-09-07 16:08:03 +0300

    did the product Details page and IProductService between server and Client

---
+++ BlazorEcommerce/Client/Service/ProductService/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using BlazorEcommerce.Shared;
+
+namespace BlazorEcommerce.Client.Service.ProductService
+{
+    public interface IProductService
+    {
+        List<Product> Products { get; set; }
+        Task GetProducts();
+    }
+}
