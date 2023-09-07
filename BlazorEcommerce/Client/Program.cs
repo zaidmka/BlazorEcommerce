49f7646a882e6941a159d1cb386c4b76ea936f4b
zaidmka <zaid.mka.90@gmail.com>
2023-09-08 01:19:03 +0300

    finish the navi (notworking yet)

--- BlazorEcommerce/Client/Program.cs
+++ BlazorEcommerce/Client/Program.cs
@@ -1,6 +1,8 @@
 global using BlazorEcommerce.Shared;
 global using System.Net.Http.Json;
 global using BlazorEcommerce.Client.Service.ProductService;
+global using BlazorEcommerce.Client.Service.CategoryService;
+
 using BlazorEcommerce.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -12,4 +14,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+
 await builder.Build().RunAsync();
---
+++ BlazorEcommerce/Client/Service/CategoryService/ICategoryService.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorEcommerce.Client.Service.CategoryService
+{
+    public interface ICategoryService
+    {
+        List<Category> Categories { get; set; }
+
+        Task GetCategories();
+    }
+}
