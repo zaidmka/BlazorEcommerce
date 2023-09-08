bb78f8ae1b96a2f690c3b567cbb8c7879b7369d4
zaidmka <zaid.mka.90@gmail.com>
2023-09-08 14:11:04 +0300

    do the price and  its variants! on details product page!

--- BlazorEcommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -14,7 +14,10 @@ namespace BlazorEcommerce.Server.Services.ProductService
         public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
         {
             var response = new ServiceResponse<Product>();
-            var product = await _context.Products.FindAsync(productId);
+            var product = await _context.Products
+                .Include(p=>p.Variants)
+                .ThenInclude(v=>v.ProductType)
+                .FirstOrDefaultAsync(p=>p.Id == productId);
             if (product == null)
             {
                 response.Success=false;
@@ -33,7 +36,7 @@ namespace BlazorEcommerce.Server.Services.ProductService
         {
             var response = new ServiceResponse<List<Product>>()
             {
-                Data = await _context.Products.ToListAsync()
+                Data = await _context.Products.Include(p => p.Variants).ToListAsync()
             };
             return response;
         }
@@ -44,6 +47,7 @@ namespace BlazorEcommerce.Server.Services.ProductService
             {
                 Data = await _context.Products
                 .Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
+                .Include(p => p.Variants)
                 .ToListAsync()
             };
 
