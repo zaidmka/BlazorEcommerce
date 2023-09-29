1f63f4c51e56943fd984307c6c00580eaa215764
zaidmka <zaid.mka.90@gmail.com>
2023-09-29 22:07:21 +0300

    Categories Administrations DONE!

--- BlazorEcommerce/Client/Service/CategoryService/ICategoryService.cs
+++ BlazorEcommerce/Client/Service/CategoryService/ICategoryService.cs
@@ -2,8 +2,20 @@
 {
     public interface ICategoryService
     {
+
+        event Action OnChange;
+
         List<Category> Categories { get; set; }
 
+        List<Category> AdminCategories { get; set; }
+
+
         Task GetCategories();
+        Task GetAdminCategories();
+        Task DeleteCategory(int categoryId);
+        Task UpdateCategory(Category category);
+        Task AddCategory(Category category);
+        Category CreateNewCategory();
+
     }
 }
